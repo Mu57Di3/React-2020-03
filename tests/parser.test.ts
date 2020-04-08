deb887239d24ae4156124fcb8a4722501ada2f6f
Bender <mu57di3@gmail.com>
2020-04-08 18:55:19 +0300

    Сдела тест парсера более коректным с проверкой на корректность строкового представления

--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -1,26 +1,27 @@
 import { Machine } from "../src/lesson_2/parser";
+import { treeToString } from "../src/lesson_2/tools";
 
 describe("Тест парсера срок", () => {
     const parser = new Machine();
     it("10 + 2", () => {
-        expect(parser.run("10 + 2").length).toBe(3);
+        expect(treeToString(parser.run("10 + 2"))).toBe("10+2");
     });
     it("4*- 2", () => {
-        expect(parser.run("4*- 2").length).toBe(3);
+        expect(treeToString(parser.run("4*- 2"))).toBe("4*-2");
     });
     it("2 * ( 3 + 2 )", () => {
-        expect(parser.run("2 * ( 3 + 2 )").length).toBe(3);
+        expect(treeToString(parser.run("2 * ( 3 + 2 )"))).toBe("2*3+2");
     });
     it("2 * ( 3 * (1 + 9) )", () => {
-        expect(parser.run("2 * ( 3 * (1 + 9) )").length).toBe(3);
+        expect(treeToString(parser.run("2 * ( 3 * (1 + 9) )"))).toBe("2*3*1+9");
     });
     it("2 * (3 *(1 +9))", () => {
-        expect(parser.run("2 * (3 *(1 +9))").length).toBe(3);
+        expect(treeToString(parser.run("2 * (3 *(1 +9))"))).toBe("2*3*1+9");
     });
     it("3**", () => {
-        expect(parser.run("3**").length).toBe(2);
+        expect(treeToString(parser.run("3**"))).toBe("3**");
     });
     it("3** +2", () => {
-        expect(parser.run("3** + 2").length).toBe(4);
+        expect(treeToString(parser.run("3** + 2"))).toBe("3**+2");
     });
 });
