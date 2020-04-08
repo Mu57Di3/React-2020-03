c5053146a572809935b00b84d313d4aef94f94e5
Bender <mu57di3@gmail.com>
2020-04-08 18:52:21 +0300

    Новая функция для отладки treeToString

--- src/lesson_2/tools.ts
+++ src/lesson_2/tools.ts
@@ -1,3 +1,27 @@
+import { Token } from "./parser";
+
+/**
+ * Проверка на тип данных число
+ * @param value
+ */
 export const isNumber = (value: string): boolean => {
     return value !== " " && !isNaN(Number(value));
+};
+
+/**
+ * Преобразует дерево токенов в строку
+ * @param tree
+ */
+export const treeToString = (tree: Array<Token>): string => {
+    let output = "";
+
+    tree.forEach((item) => {
+        if (item.type !== "Quote") {
+            output += String(item.number || item.operator);
+        } else {
+            output += treeToString(item.children);
+        }
+    });
+
+    return output;
 };
