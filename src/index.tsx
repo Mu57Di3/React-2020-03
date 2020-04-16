92b35e7333d5d1d1c301fa114d44939a96542b26
Bender <mu57di3@gmail.com>
2020-04-16 14:21:07 +0300

    Индексный файл приложения

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,25 @@
 import React from "react";
 import { render } from "react-dom";
 
-render(<h1>Hello world</h1>, document.getElementById("root"));
+import "bootstrap/dist/css/bootstrap.min.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import ToDoApp from "./containers/ToDoApp";
+
+library.add(fab, far, fas);
+
+const App: React.FC<{}> = () => {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-6">
+                    <ToDoApp />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+render(<App />, document.getElementById("root"));
